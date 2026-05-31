6fe6fce6c92db846475d629a5306118ceb8d6785
ngutech21 <ngutech21@pm.me>
2026-05-31 16:44:30 +0200

    Initial Commit

---
+++ local-coder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  local-coder
+//
+//  Created by Steffen Kriese on 31.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ local-coder/local_coderApp.swift
@@ -0,0 +1,17 @@
+//
+//  local_coderApp.swift
+//  local-coder
+//
+//  Created by Steffen Kriese on 31.05.26.
+//
+
+import SwiftUI
+
+@main
+struct local_coderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
