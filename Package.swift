9da58f5f3676701bbe0c44640117f6ba6c15f405
ngutech21 <ngutech21@pm.me>
2026-08-14 14:01:26 +0200

    chore(deps): update package revisions for mlx-swift-lm and SwiftTerm

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
     .package(url: "https://github.com/ml-explore/mlx-swift/", from: "0.31.6"),
     .package(
       url: "https://github.com/ml-explore/mlx-swift-lm",
-      revision: "01472a78fca830689ff78246a82c6d31ab111a78"),
+      revision: "d66761008139f4312269d9e931f69a404f2d1330"),
     .package(url: "https://github.com/huggingface/swift-transformers", from: "1.3.3"),
     .package(url: "https://github.com/huggingface/swift-huggingface", from: "0.9.0"),
     .package(url: "https://github.com/FluidInference/FluidAudio", from: "0.15.4"),
