fd08be1231f73fe0ae11e4e1997c3875228880a9
ngutech21 <ngutech21@pm.me>
2026-08-28 08:37:38 +0200

    chore: update revision for mlx-swift-lm dependency in Package.resolved

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
     .package(url: "https://github.com/ml-explore/mlx-swift/", from: "0.31.6"),
     .package(
       url: "https://github.com/ml-explore/mlx-swift-lm",
-      revision: "6745899c2d6fd2c2325055005f1d463237d44153"),
+      revision: "37688d2cf7d3906e08c74479c9d9949ce6b81136"),
     .package(url: "https://github.com/huggingface/swift-transformers", from: "1.3.3"),
     .package(url: "https://github.com/huggingface/swift-huggingface", from: "0.9.0"),
     .package(
